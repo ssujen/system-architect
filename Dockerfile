50c5186815b55323114127e12da6c6ed430f3fea
ssujen <ssujenlu@gmail.com>
2026-03-03 15:58:37 -0800

    removed Docker reference

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Build Stage
-FROM node:20-slim 
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run build
-EXPOSE 8080
-CMD ["npm", "run", "dev"]
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud run deploy system-architect-ai \
-  --source . \
-  --region us-central1 \
-  --allow-unauthenticated \
-  --set-env-vars="GEMINI_API_KEY=AIzaSyAGvPgE2cJY74NElSdFPtILuXazyn14nwQ"
